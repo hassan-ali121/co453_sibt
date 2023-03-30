acdf51c5310d70842de835f7a0d09a64d0d919da
hassan-ali121 <110232196+hassan-ali121@users.noreply.github.com>
2023-03-30 22:43:18 +0100

    Update Network.cs

--- ConsoleAppProject/App04/Network.cs
+++ ConsoleAppProject/App04/Network.cs
@@ -1,1 +1,52 @@
+using System;
+using ConsoleAppProject.Helpers;
+
+namespace ConsoleAppProject.App04
+{
+    public class Network
+    {
+        private NewsFeed news = new NewsFeed();
+       
+        public void DispalyMenu()
+        {
+            Console.WriteLine("which app you want to open");
+            Console.WriteLine(" Select any of the number : ");
+            Console.WriteLine("1. message post");
+            Console.WriteLine("2. photo post");
+            Console.WriteLine("3. Display all posts");
+
+            Console.WriteLine("Please Enter any number");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                postMessage();
+            }
+            else if (choice == "2")
+            {
+                PostPhoto();
+            }
+            else
+            {
+                postAll();
+            }
+
+        }
+
+        private void postAll()
+        {
+            news.Display();
+            news.DisplayLikedPosts();
+        }
+
+        private void PostPhoto()
+        {
+        }
+
+        private void postMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
 
