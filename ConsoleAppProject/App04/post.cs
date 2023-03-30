f9ec1bcc1bbf0a9511df6c15faa8c0bd831f274c
hassan-ali121 <110232196+hassan-ali121@users.noreply.github.com>
2023-03-30 22:47:24 +0100

    Update post.cs

--- ConsoleAppProject/App04/post.cs
+++ ConsoleAppProject/App04/post.cs
@@ -1,1 +1,108 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppProject.App04
+{
+	public class post
+	{
+
+        public int likes;
+
+        private readonly List<String> comments;
+
+
+        
+        public String Username { get; }
+
+        public DateTime Timestamp { get; }
+
+        public post(string author) {
+
+            this.Username=author;
+            Timestamp = DateTime.Now;
+            likes = 0;
+            comments = new List<String>();
+        }
+        /// <summary>
+        /// Record one more 'Like' indication from a user.
+        /// </summary>
+        public void Like()
+        {
+            likes++;
+        }
+
+        ///<summary>
+        /// Record that a user has withdrawn his/her 'Like' vote.
+        ///</summary>
+        public void Unlike()
+        {
+            if (likes > 0)
+            {
+                likes--;
+            }
+        }
+
+        ///<summary>
+        /// Add a comment to this post.
+        /// </summary>
+       
+        /// The new comment to add.
+        /// </param>        
+        public void AddComment(String text)
+        {
+            comments.Add(text);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void Display()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"    Author: {Username}");
+          
+            Console.WriteLine($"    Time Elpased: {FormatElapsedTime(Timestamp)}");
+            Console.WriteLine();
+
+            if (likes > 0)
+            {
+                Console.WriteLine($"    Likes:  {likes}  people like this.");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            if (comments.Count == 0)
+            {
+                Console.WriteLine("    No comments.");
+            }
+            else
+            {
+                Console.WriteLine($"    {comments.Count}  comment(s). Click here to view.");
+            }
+        }
+        
+        private String FormatElapsedTime(DateTime time)
+        {
+            DateTime current = DateTime.Now;
+            TimeSpan timePast = current - time;
+
+            long seconds = (long)timePast.TotalSeconds;
+            long minutes = seconds / 60;
+
+            if (minutes > 0)
+            {
+                return minutes + " minutes ago";
+            }
+            else
+            {
+                return seconds + " seconds ago";
+            }
+        }
+
+
+    }
+}
+
 
